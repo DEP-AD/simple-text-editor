e86f4600ab049dcca7581f032546696b7a19cead
Aruni Dissanayake <aruni.saku@gmail.com>
2021-06-25 01:52:08 +0530

    coding btnFindNext method

--- src/controller/TextEditorFormController.java
+++ src/controller/TextEditorFormController.java
@@ -5,11 +5,16 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TextEditorFormController {
 
     public TextArea txtEditor;
     public AnchorPane pneFind;
     public TextField txtSearch;
+
+    private int findOffSet = 0;
 
     public void initialize(){
         pneFind.setVisible(false);
@@ -37,6 +42,19 @@ public class TextEditorFormController {
     }
 
     public void btnFindNext_OnAction(ActionEvent actionEvent) {
+        String pattern = txtSearch.getText();
+        String text = txtEditor.getText();
+
+        Pattern regExp= Pattern.compile(pattern);
+        Matcher matcher = regExp.matcher(text);
+
+        boolean exits = matcher.find(findOffSet);
+        if(exits){
+            findOffSet = matcher.start()+1;
+            txtEditor.selectRange(matcher.start(), matcher.end());
+        }
+
+
     }
 
     public void btnFindPreview_OnAction(ActionEvent actionEvent) {
