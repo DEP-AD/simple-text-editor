b0007437191caf672db0e2e32f3ad888aa5a08f9
Aruni Dissanayake <aruni.saku@gmail.com>
2021-06-26 15:56:36 +0530

    finilized find meethod

--- src/controller/TextEditorFormController.java
+++ src/controller/TextEditorFormController.java
@@ -5,8 +5,12 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
+import java.sql.ClientInfoStatus;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 public class TextEditorFormController {
 
@@ -14,11 +18,27 @@ public class TextEditorFormController {
     public AnchorPane pneFind;
     public TextField txtSearch;
 
-    private int findOffSet = 0;
+    private int findOffSet = -1;
+    private List<Index> searchList   = new ArrayList<>();
+    private int searchIndex = 0;
 
     public void initialize(){
         pneFind.setVisible(false);
+        txtSearch.textProperty().addListener((observable, oldValue, newValue) -> {
+            try {
+                Pattern regExp = Pattern.compile(newValue);
+                Matcher matcher = regExp.matcher(txtEditor.getText());
 
+                searchList.clear();
+
+                while (matcher.find()) {
+                    searchList.add(new Index(matcher.start(), matcher.end()));
+                }
+            }
+            catch(PatternSyntaxException e){
+
+            }
+        });
     }
 
     public void mnuItemNew_OnAction(ActionEvent actionEvent) {
@@ -42,21 +62,38 @@ public class TextEditorFormController {
     }
 
     public void btnFindNext_OnAction(ActionEvent actionEvent) {
-        String pattern = txtSearch.getText();
-        String text = txtEditor.getText();
-
-        Pattern regExp= Pattern.compile(pattern);
-        Matcher matcher = regExp.matcher(text);
-
-        boolean exits = matcher.find(findOffSet);
-        if(exits){
-            findOffSet = matcher.start()+1;
-            txtEditor.selectRange(matcher.start(), matcher.end());
+        if(!searchList.isEmpty()) {
+            if(findOffSet==-1){
+                findOffSet=0;
+            }
+            txtEditor.selectRange(searchList.get(findOffSet).startingIndex, searchList.get(findOffSet).endIndex);
+            findOffSet++;
+            if(findOffSet>=searchList.size()){
+                findOffSet=0;
+            }
         }
-
-
     }
 
     public void btnFindPreview_OnAction(ActionEvent actionEvent) {
+        if(!searchList.isEmpty()) {
+            if(findOffSet==-1){
+                findOffSet=searchList.size()-1;
+            }
+            txtEditor.selectRange(searchList.get(findOffSet).startingIndex, searchList.get(findOffSet).endIndex);
+            findOffSet--;
+            if(findOffSet<0){
+                findOffSet=searchList.size()-1;
+            }
+        }
+    }
+}
+
+class Index{
+    int startingIndex;
+    int endIndex;
+
+    public Index(int startingIndex, int endIndex) {
+        this.startingIndex = startingIndex;
+        this.endIndex = endIndex;
     }
 }
